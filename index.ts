9de83e3136605d275a6162b2c255db19742b9962
Muhammad Bilal Khan <160259391+Bilalkhan0026@users.noreply.github.com>
2024-03-22 15:40:32 -0700

    created numbar game

---
+++ index.ts
@@ -0,0 +1,16 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const randomNumbar = Math.floor(Math.random() * 6 + 1);
+const answers = await inquirer.prompt([
+    {
+       name: "userGuessedNumbar",
+       type: "number",
+       message: "Please guess a numbar between 1-6:",
+    },
+]);
+
+  if(answers.userGuessedNumbar === randomNumbar ){
+    console.log("congtratulation! you guessed right a numbar ");
+  }else{
+    console.log("you gussed wrong numbar");
+  }
